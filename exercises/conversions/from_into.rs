cc6d9d3a7f34fb8fbaad766124dad2c455920c8c
kuraxii <zj.zhu.cn@gmail.com>
2024-10-06 15:44:30 +0800

    conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -7,6 +7,8 @@
 // Execute `rustlings hint from_into` or use the `hint` watch subcommand for a
 // hint.
 
+use std::{usize, vec};
+
 #[derive(Debug)]
 struct Person {
     name: String,
@@ -40,10 +42,38 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty(){
+            return Person::default();
+        }
+        let parts: Vec<&str> = s.split(',').collect();
+        if(parts.len() != 2){
+            return Person::default();
+
+        }
+
+
+        let name = parts[0].trim();
+        let age = parts[1].trim();
+
+        if name.is_empty() || age.is_empty()  {
+            return Person::default();
+        }
+
+        if let Ok(age) = age.parse::<usize>(){
+            Person{
+                name: name.into(),
+                age: age
+            }
+        }else{
+             Person::default()
+
+        }
+       
+    
     }
 }
 
